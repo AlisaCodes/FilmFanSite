d470fe3612a10e4e1f204efd0d5c6cfbef0e3199
alisa <alisaclone@gmail.com>
2015-10-15 15:14:24 -0700

    add the styling to images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+    films = Film.create([{title: "Raise the Red Lantern", url: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQN5B79KXmcvCara6yovOF136z07Vz32SUZq57ThSDngO40Msl6", director: "Yimou Zhang" }, {title: "Last Life in the Universe", director: "Pen-Ek Ratanaruang", url: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQN5B79KXmcvCara6yovOF136z07Vz32SUZq57ThSDngO40Msl6"}, {title: "The Abyss", director: "James Cameron", url: "https://alienationmentale.files.wordpress.com/2011/10/abyss.jpg"}])
